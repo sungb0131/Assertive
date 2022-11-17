736f26f32993e184d42a0d8b6a0dd2b7a82cb858
G4tsby <juwonkwon.dev@gmail.com>
2022-11-17 17:36:23 +0900

    CHORE: Add server to allowed host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret['django']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['g4tsby.xyz']
 
 
 # Application definition
