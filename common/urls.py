3b43a73f800d5595bd579c399faa11c468e94bb1
G4tsby <juwonkwon.dev@gmail.com>
2022-12-10 23:01:15 +0900

    STYLE: Change login page style

--- common/urls.py
+++ common/urls.py
@@ -4,7 +4,7 @@ from common import views
 
 app_name = 'common'
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login1.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('security/', views.security, name='signup'),
+    path('security/', views.security, name='security'),
     ]
--- common/views.py
+++ common/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from main.models import SecurityTodo
 
 def security(request):
-    return render(request, 'security.html')
+    return render(request, 'security.html', {
+        'todos': SecurityTodo.objects.all().order_by('-date')
+    })
