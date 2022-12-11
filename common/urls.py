af6f42e5d8930d27b2a39c86cf69b81112c1a672
G4tsby <juwonkwon.dev@gmail.com>
2022-12-11 18:37:36 +0900

    FEAT: Edit parking

--- common/urls.py
+++ common/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('security/', views.security, name='security'),
+    path('security/todo/<int:todo_id>/<int:status>', views.todo, name='todo')
     ]
--- common/views.py
+++ common/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import SecurityTodo
 
 def security(request):
+    if not request.user.is_staff:
+        return redirect('/')
     return render(request, 'security.html', {
-        'todos': SecurityTodo.objects.all().order_by('-date')
+        'todos': SecurityTodo.objects.all().order_by('date', 'status')
     })
+
+def todo(request, todo_id, status):
+    todo = SecurityTodo.objects.get(id=todo_id)
+    todo.status = status
+    todo.save()
+    return redirect('/security')
