f8cdc57e798160e4562b0cf5d21b8a2e3788d7e9
G4tsby <juwonkwon.dev@gmail.com>
2022-12-12 21:37:24 +0900

    FEAT: Add conv

--- main/models.py
+++ main/models.py
@@ -4,15 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 #main date
 class Calender(models.Model):
+    class Meta:
+        verbose_name_plural = "일정"
     date = models.DateField()
     content = models.CharField(max_length=30)
 
 class User(AbstractUser):
+    class Meta:
+        verbose_name_plural = "사용자"
     car = models.CharField(max_length=8, null=True)
     phone = models.CharField(max_length=11)
 
 #convenience0
 class Convenient(models.Model):
+    class Meta:
+        verbose_name_plural = "편의시설"
     title = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
     content = models.TextField()
@@ -22,6 +28,8 @@ class Convenient(models.Model):
         return f"{self.title}"
 
 class Fee(models.Model):
+    class Meta:
+        verbose_name_plural = "요금"
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     kind = models.CharField(max_length=50)
     fee = models.IntegerField()
@@ -34,6 +42,8 @@ class Fee(models.Model):
         return f"{self.type_dict[self.kind]}요금 {self.user.username}님 {self.date_year}년 {self.date_month}월"
 
 class SecurityTodo(models.Model):
+    class Meta:
+        verbose_name_plural = "경비 업무"
     date = models.DateTimeField()
     deadline = models.DateTimeField()
     title = models.CharField(max_length = 20)
@@ -45,6 +55,8 @@ class SecurityTodo(models.Model):
 
 #commuity
 class Board(models.Model):
+    class Meta:
+        verbose_name_plural = "게시판"
     name = models.CharField(max_length=50)
     description = models.TextField()
 
@@ -60,15 +72,20 @@ class Post(models.Model):
     isAnonymous = models.BooleanField()
 
 class CommunityPost(Post):
-    pass
+    class Meta:
+        verbose_name_plural = "게시판 글"
 
     def __str__(self):
         return f"{self.board}|{self.title}"
 
 class Comment(Post):
+    class Meta:
+        verbose_name_plural = "댓글"
     post = models.ForeignKey(CommunityPost, on_delete=models.CASCADE)
 
 class Notification(models.Model):
+    class Meta:
+        verbose_name_plural = "사용자별 알림"
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField()
     title = models.CharField(max_length=20)
@@ -80,6 +97,8 @@ class Parking(models.Model):
     space = models.IntegerField()
 
 class Visitor(models.Model):
+    class Meta:
+        verbose_name_plural = "방문차량"
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateField()
     car = models.CharField(max_length=8)
